2fb2a5960a7f58c05623277504ea0c1264255d57
Sinjoy Saha <sinjoysaha555@gmail.com>
2019-01-11 00:45:54 +0530

    Modified with on_status

--- chandler_the_bot.py
+++ chandler_the_bot.py
@@ -1,69 +1,28 @@
 from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-from tweepy import API
-import json
-import time
 from config import *
+from functions_on_status import *
+from tweepy import OAuthHandler
+from tweepy import API
 
-'''# consumer key, consumer secret, access token, access secret.
-ckey = cfg.ckey
-csecret = cfg.csecret
-atoken = cfg.atoken
-asecret = cfg.asecret'''
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+api = API(auth)
 
-tweet_dict = {}
-
-
-def processTweet(tweet_data):
-    tweet_dict = json.loads(tweet_data)
-    tweet_time = tweet_dict["created_at"]
-    tweet_user_name = tweet_dict["user"]["name"]
-    tweet_user_screen_name = tweet_dict["user"]["screen_name"]
-    tweet_text = tweet_dict["text"]
-    tweet_fulltext = tweet_text
-
-    if(tweet_dict["truncated"]):
-        tweet_fulltext = tweet_dict["extended_tweet"]["full_text"]
-
-    if(tweet_text[:4] == 'RT @'):
-        tweet_fulltext = tweet_dict["retweeted_status"]["text"]
-
-        if(tweet_dict["retweeted_status"]["truncated"]):
-            tweet_fulltext = tweet_dict["retweeted_status"]["extended_tweet"]["full_text"]
-
-    time.sleep(2)
-    print()
-    print("Time:\t\t\t" + tweet_time)
-    print("User Name:\t\t\t" + tweet_user_name)
-    print("User Screen Name:\t\t\t@" + tweet_user_screen_name)
-    print("Text:\t\t\t" + tweet_text)
-    print("Full Text:\t\t\t" + tweet_fulltext)
-    print()
-    time.sleep(2)
 
 def postTweet(tweet_this):
     api.update_status(tweet_this)
     print("TWEETED:", tweet_this)
 
 
-class listener(StreamListener):
+# consumer key, consumer secret, access token, access secret.
 
-    def on_data(self, data):
-        print(data)
-        processTweet(data)
-        return(True)
-
-    def on_error(self, status):
-        print(status)
-
-
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
-api = API(auth)
-
-tweet_this = 'Test Tweet 4'
-postTweet(tweet_this)
+# tweet_this = 'Test Tweet 6'
+# postTweet(tweet_this)
 
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["chandler"])
+twitterStream.filter(track=["#sarcasm", "#sarcastic"])
+
+'''timeline = api.home_timeline()
+for time_tweet in timeline:
+    print(time_tweet.text)
+'''
